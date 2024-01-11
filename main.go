21a423aaf526977c9403d5ad76f0a213c4511a75
626 <52466421+D2372017513@users.noreply.github.com>
2024-01-11 15:22:07 +0800

    增加 about 分支检测

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import (
 )
 
 func handFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello goblog")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "根目录")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "about 目录")
+	} else {
+		fmt.Fprint(w, "404")
+	}
+	// fmt.Fprintf(w, "hello goblog")
 }
 
 func main() {
