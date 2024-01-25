c5b590f6c42a83a8da0f8c62d13b1e054dc854c3
626 <52466421+D2372017513@users.noreply.github.com>
2024-01-25 15:48:03 +0800

    重构 Int64ToString

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"goblog/logger"
 	"goblog/route"
 	blogSql "goblog/sql"
+	"goblog/types"
 )
 
 var router *mux.Router
@@ -46,11 +47,6 @@ func (a ArticlesData) Delete() (rowsAffected int64, err error) {
 	}
 
 	return 0, nil
-}
-
-// Int64ToString 将 int64 转换为 string
-func Int64ToString(num int64) string {
-	return strconv.FormatInt(num, 10)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -88,7 +84,7 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 		// 4. 读取成功
 		tmpl, err := template.New("show.gohtml").Funcs(template.FuncMap{
 			"RouteName2URL": route.Name2URL,
-			"Int64ToString": Int64ToString,
+			"Int64ToString": types.Int64ToString,
 		}).ParseFiles("resources/views/articles/show.gohtml")
 		logger.LogErr(err)
 		tmpl.Execute(w, rs)
